862ff64b9bf9cfb0f021450c7d02480b707f4af2
Yung-Che <senseye935@gmail.com>
2024-03-03 12:45:47 +0800

    feat: add deposit test file

--- src/tests/account.test.ts
+++ src/tests/account.test.ts
@@ -13,6 +13,8 @@ afterAll((done) => {
 });
 
 describe("Account API", () => {
+  let createdAccountId: string;
+
   it("should create a new account", async () => {
     const res = await request(app).post("/api/accounts").send({
       name: "John Doe",
@@ -20,7 +22,18 @@ describe("Account API", () => {
     });
     expect(res.statusCode).toEqual(201);
     expect(res.body).toHaveProperty("id");
+    createdAccountId = res.body.id;
     expect(res.body.name).toEqual("John Doe");
     expect(res.body.balance).toEqual(1000);
+  });
+
+  it("should deposit money to an account", async () => {
+    const depositAmount = 500;
+    const res = await request(app).post(`/api/accounts/deposit`).send({
+      id: createdAccountId,
+      amount: depositAmount,
+    });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.balance).toEqual(1500);
   });
 });
